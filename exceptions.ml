d91c5067c1bca65a596440da91859cd3257a6cc0
Lalitha Madduri <lm3302@columbia.edu>
2020-04-22 16:06:25 -0400

    added exceptions file

---
+++ exceptions.ml
@@ -0,0 +1,13 @@
+exception MissingEOF
+exception Invalid
+exception InvalidBinaryOperation
+exception InvalidAssignment
+exception WrongNumberOfArguments
+exception IncorrectArgumentType of string
+exception IllegalBinOp of string
+exception IllegalUnOp of string * string * string
+exception IncorrectType of string
+exception UnrecognizedFunction of string
+exception NonAccessibleProp of string
+exception IllegalArgument
+exception IllegalCondition
