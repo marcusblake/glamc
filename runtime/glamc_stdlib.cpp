a01164c1cf3c9dbaef719d57f002708e516efade
Claire Jenkins <61124312+clj2142@users.noreply.github.com>
2020-05-13 23:39:48 -0400

    type conversions

--- runtime/glamc_stdlib.cpp
+++ runtime/glamc_stdlib.cpp
@@ -1,5 +1,6 @@
 #include "glamc_stdlib.h"
 #include <stdio.h>
+#include <string.h>
 
 extern "C" void printb(int d) {
     if (d == 0) {
@@ -7,4 +8,28 @@ extern "C" void printb(int d) {
     } else {
         printf("%s\n", "true");
     }
+}
+
+extern "C" int string_to_int(string s) {
+    return atoi(s);
+}
+
+extern "C" string int_to_string(int i) {
+    return to_string(i);
+}
+
+extern "C" int float_to_int(float f) {
+    return (int)f;
+}
+
+extern "C" float int_to_float(int i) {
+    return (float)i;
+}
+
+extern "C" int char_to_int(char c) {
+    return (int)c;
+}
+
+extern "C" char int_to_char(int i) {
+    return (char)i;
 }
